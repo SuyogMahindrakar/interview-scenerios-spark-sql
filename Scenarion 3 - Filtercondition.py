ae0c441b6dd243a43d5e7134ae2060c88dbfdf8c
SuyogMahindrakar <suyog.mahindrakar@gmail.com>
2025-04-04 15:39:18 +0800

    Scenario 3 - Filtercondition.py

---
+++ Scenarion 3 - Filtercondition.py
@@ -0,0 +1,29 @@
+!pip install pyspark
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Colab_PySpark").getOrCreate()
+
+data = [
+    (1, "Alice", "IT", 5000, 2),
+    (2, "Bob", "HR", 6000, 5),
+    (3, "Alice", "IT", 5000, 2),
+    (4, "David", "Finance", 7000, 7),
+    (5, "Eva", "IT", 8000, 6),
+    (6, "Bob", "HR", 6000, 5),
+    (7, "Grace", "Finance", 9000, 10)
+]
+
+# Create DataFrame
+columns = ["id", "name", "department", "salary", "experience"]
+df = spark.createDataFrame(data, columns);
+df.show()
+
+#For single column filtering
+sincol = df.filter("department='IT'")
+sincol.show()
+
+doublecol = df.filter("department='IT' and salary > 5000")
+doublecol.show()
