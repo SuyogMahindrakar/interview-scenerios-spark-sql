04bdc525b735e8c05e88824120f73d23f75c7f1b
SuyogMahindrakar <suyog.mahindrakar@gmail.com>
2025-04-04 17:51:09 +0800

    Update Scenarion 3 - Filtercondition.py

--- Scenarion 3 - Filtercondition.py
+++ Scenarion 3 - Filtercondition.py
@@ -13,7 +13,8 @@ data = [
     (4, "David", "Finance", 7000, 7),
     (5, "Eva", "IT", 8000, 6),
     (6, "Bob", "HR", 6000, 5),
-    (7, "Grace", "Finance", 9000, 10)
+    (7, "Grace", "Finance", 9000, 10),
+    (8, "suyog",None, 9000, 10)
 ]
 
 # Create DataFrame
@@ -26,4 +27,21 @@ sincol = df.filter("department='IT'")
 sincol.show()
 
 doublecol = df.filter("department='IT' and salary > 5000")
+doublecol.show()
+
+#in
+
+doublecol = df.filter("department in ('IT','HR')")
+doublecol.show()
+
+#like 
+doublecol = df.filter("name like 'A%'")
+doublecol.show()
+
+#check is null  
+doublecol = df.filter("department is NULL")
+doublecol.show()
+
+#check is not null  
+doublecol = df.filter("department is not null")
 doublecol.show()
